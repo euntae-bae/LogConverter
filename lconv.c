3ef2f33370a510ca3f9da8028de0427985a0ec68
euntae-bae <euntae471@gmail.com>
2021-05-27 06:43:59 +0100

    Update: lconv2 to lconv2a - calculate velocity and its norm

--- lconv.c
+++ lconv.c
@@ -7,16 +7,21 @@
 
 int main(int argc, char *argv[])
 {
-    pid_t pid;
+    //pid_t pid;
     const char *finName[3] = { "sensor-ax.txt", "sensor-ay.txt", "sensor-az.txt" };
-    char str[256];
+    char cmd[256];
     int i;
 
     printf("## lconv front-end ##\n");
-    if (argc != 2) {
-        printf("usage: %s output-file\n", argv[0]);
+    if (argc == 2) {
+        sprintf(cmd, "mkdir %s", argv[1]);
+        system(cmd);
+    }
+    else if (argc > 2) {
+        printf("usage: %s [dest-directory]\n", argv[0]);
         return -1;
     }
+    
     /*
     for (i = 0; i < 3; i++) {
         pid = fork();
@@ -35,13 +40,18 @@ int main(int argc, char *argv[])
     execl("./lconv2", argv[1], NULL); */
 
     for (i = 0; i < 3; i++) {
-        strcpy(str, "./lconv1 ");
-        strcat(str, finName[i]);
-        system(str);
+        strcpy(cmd, "./lconv1 ");
+        strcat(cmd, finName[i]);
+        system(cmd);
     }
-    strcpy(str, "./lconv2 ");
-    strcat(str, argv[1]);
-    system(str);
+    //strcpy(cmd, "./lconv2 ");
+    //strcat(cmd, argv[1]);
+    system("./lconv2");
+    
+    if (argc == 2) {
+        sprintf(cmd, "mv *.txt ./%s", argv[1]);
+        system(cmd);
+    }
     
     return 0;
 }
--- lconv2.c
+++ lconv2.c
@@ -4,6 +4,8 @@
 #include <ctype.h>
 #include <math.h>
 #define BUF_SIZE 256
+#define FOUT1_NAME	"sensor-norm.txt"
+#define FOUT2_NAME	"sensor-intg.txt"
 
 double getNorm(double x, double y, double z) {
 	return sqrt((x * x) + (y * y) + (z * z));
@@ -12,29 +14,31 @@ double getNorm(double x, double y, double z) {
 int main(int argc, char *argv[])
 {
 	char finName[3][BUF_SIZE];
-	char foutName[BUF_SIZE];
 	FILE *fin[3] = { NULL, };
-	FILE *fout = NULL;
+	FILE *fout1 = NULL;
+	FILE *fout2 = NULL;
 
 	int i;
 	double timeCnt = 0.0;
 	double ax, ay, az;
 	double norm;
 
-	printf("## log converter version 2 ##\n");
-	if (argc == 2) {
+	const double dt = 1 / 50.0;
+	const double constTerm = 9.8 * dt / 2;
+	double vx, vy, vz, vnorm;
+
+	printf("## log converter version 2a ##\n");
+	if (argc == 1) {
 		strcpy(finName[0], "sensor-ax-out.txt");
 		strcpy(finName[1], "sensor-ay-out.txt");
 		strcpy(finName[2], "sensor-az-out.txt");
-		strcpy(foutName, argv[1]);
 	}
-	else if (argc == 5) {
+	else if (argc == 4) {
 		for (i = 0; i < 3; i++)
 			strcpy(finName[i], argv[i + 1]);
-		strcpy(foutName, argv[4]);
 	}
 	else {
-		printf("usage: %s [input-x input-y input-z] output-file\n", argv[0]);
+		printf("usage: %s [input-x input-y input-z]\n", argv[0]);
 		return -1;
 	}
 
@@ -45,13 +49,18 @@ int main(int argc, char *argv[])
 			return -1;
 		}
 	}
-	fout = fopen(foutName, "wt");
-	if (!fout) {
-		fprintf(stderr, "could not open file: %s\n", foutName);
+	fout1 = fopen(FOUT1_NAME, "wt");
+	fout2 = fopen(FOUT2_NAME, "wt");
+	if (!fout1) {
+		fprintf(stderr, "could not open file: %s\n", FOUT1_NAME);
+		return -1;
+	}
+	if (!fout2) {
+		fprintf(stderr, "could not open file: %s\n", FOUT2_NAME);
 		return -1;
 	}
 	printf("input files: %s %s %s\n", finName[0], finName[1], finName[2]);
-	printf("output file: %s\n", foutName);
+	printf("output file: %s %s\n", FOUT1_NAME, FOUT2_NAME);
 
 	/*
 	for (i = 0; i < 5; i++) {
@@ -60,21 +69,29 @@ int main(int argc, char *argv[])
 	printf("\n");
 	*/
 
-	double dtime;
+	double blankTime;
+	vx = 0.0, vy = 0.0, vz = 0.0;
+
 	while (!feof(fin[0])) {
 		//fgets(buf[0], BUF_SIZE, fin[0]);
-		fscanf(fin[0], "%lf\t%lf\n", &dtime, &ax);
-		fscanf(fin[1], "%lf\t%lf\n", &dtime, &ay);
-		fscanf(fin[2], "%lf\t%lf\n", &dtime, &az);
+		fscanf(fin[0], "%lf\t%lf\n", &blankTime, &ax);
+		fscanf(fin[1], "%lf\t%lf\n", &blankTime, &ay);
+		fscanf(fin[2], "%lf\t%lf\n", &blankTime, &az);
 		norm = getNorm(ax, ay, ax);
 		//printf("%.2lf\t%lf %lf %lf %lf\n", timeCnt, ax, ay, az, norm);
 
-		fprintf(fout, "%.2lf\t%lf\n", timeCnt, norm);
+		fprintf(fout1, "%.2lf\t%lf\n", timeCnt, norm);
+
 		timeCnt += 0.02;
+		vx += ax * constTerm;
+		vy += ay * constTerm;
+		vz += az * constTerm;
+		vnorm = getNorm(vx, vy, vz);
+		fprintf(fout2, "%.2lf\t%lf\t%lf\t%lf\t%lf\n", timeCnt, vz / timeCnt, vy / timeCnt, vz / timeCnt, vnorm / timeCnt);
 	}
 
 	for (i = 0; i < 3; i++)
 		fclose(fin[i]);
-	fclose(fout);
+	fclose(fout1);
 	return 0;
 }
